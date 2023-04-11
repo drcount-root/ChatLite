23b89e11ead2ebc66849514cf4b457b2bc4c779d
drcount-root <about.subhamsahu@gmail.com>
2023-04-11 17:19:13 +0530

    without css

---
+++ BE/server.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const express = require("express");
+const socketIO = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on("connection", (socket) => {
+  console.log("a user is connected");
+
+  socket.on("message", (data) => {
+    console.log("message received: " + data);
+    io.emit("message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on port: *3000");
+});
